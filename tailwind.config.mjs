5c72bede389a0a8ba679aa08ef578f72201995d2
nasa14119 <nico.amaya0205@gmail.com>
2024-04-02 19:04:44 -0600

    feat(Blog): Implimenting all the blog basicly adding layout, css, test post

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,14 +3,17 @@ import animations from '@midudev/tailwind-animations'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors:{
-			primary: "var(--primary)", 
-			secundary: "var(--secundary)",
-			complementary:"var(--complementary)", 
-			complementOne: "var(--complementOne)", 
-			complementTwo:"var(--complementTwo)", 
-		},
 		extend: {
+			colors:{
+				primary: "var(--primary)", 
+				secundary: "var(--secundary)",
+				complementary:"var(--complementary)", 
+				complementOne: "var(--complementOne)", 
+				complementTwo:"var(--complementTwo)", 
+				black:"#000",
+				white:"#fff",
+				transparent:"transparent"
+			},
 			fontFamily: {
 			  'h1': ['var(--primary-font)'],
 			  'p': ['var(--paragraft-font)'],
