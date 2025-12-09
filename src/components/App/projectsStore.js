ee1701bcd25e7df343d2706be4462b9b7bdc3df2
nasa14119 <nico.amaya0205@gmail.com>
2025-12-09 17:22:40 -0600

    feat(modal): changes in modal design

--- src/components/App/projectsStore.js
+++ src/components/App/projectsStore.js
@@ -1,5 +1,5 @@
-import { atom } from 'nanostores';
+import { atom } from "nanostores";
 
 export const projectIndex = atom(0);
 export const loader = atom(false);
-export const modal = atom(false); 
+export const modal = atom("idle");
--- src/components/ProjectDescription/components/MenuProyects/utils.ts
+++ src/components/ProjectDescription/components/MenuProyects/utils.ts
@@ -1,17 +1,18 @@
-import { IMPORTS } from "@assets/data"
-import { projectIndex } from "../../../App/projectsStore"
-import { modal } from "../../../App/projectsStore.js"
-export const changePage = (section:number) => {
-  projectIndex.set(section)
-}
-export const setModal = () =>{ 
-  modal.set(!modal.get());
-} 
-type Info = Array<{title: string, id: number}> 
-export const getTitles = ():Info =>{
-  let resutl:Info= []
-  IMPORTS.forEach(v =>{
-    resutl.push({title: v.title,id: v.id}) 
-  })
-  return resutl
-} 
+import { IMPORTS } from "@assets/data";
+import { projectIndex } from "../../../App/projectsStore";
+import { modal } from "../../../App/projectsStore.js";
+export const changePage = (section: number) => {
+  projectIndex.set(section);
+};
+export const setModal = () => {
+  if (modal.get() === "idle") return modal.set("true");
+  modal.set(modal.get() === "true" ? "false" : "true");
+};
+type Info = Array<{ title: string; id: number }>;
+export const getTitles = (): Info => {
+  let resutl: Info = [];
+  IMPORTS.forEach((v) => {
+    resutl.push({ title: v.title, id: v.id });
+  });
+  return resutl;
+};
